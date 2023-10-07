c1d318b7db472e48e9bc5cc31ced4d49b19670c3
jyoti ranjan sharma <jyotiranjansharma11@gmail.com>
2023-10-07 23:00:08 +0530

    add filterer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Toggle from './components/Toggle'
 import Timer from './components/Timer'
 import Counter from './components/Counter'
+import Filterer from './components/Filterer'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [options, setOptions] = useState<string[]>([])
+
+  const selectChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+    if (!options.includes(e.target.value)){
+      setOptions([e.target.value])
+    }
+    
+  }
 
   return (
     <>
@@ -60,6 +69,18 @@ function App() {
             )
           }}
         </Counter>
+      </div>
+      <div>
+        <select onChange={(e) => selectChangeHandler(e)}>
+          <option value="all">all</option>
+          <option value="active">active</option>
+          <option value="completed">completed</option>
+        </select>
+        <Filterer options={options}>
+          {(data) => {
+            return data.map((item) => <p key={item.id}>{item.task}</p>)
+          }}
+        </Filterer>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
